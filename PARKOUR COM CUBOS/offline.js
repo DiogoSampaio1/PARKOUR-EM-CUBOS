7169e6492e4511b4c200eeb8879a9f5a38de2d90
DiogoSampaio1 <sampaio.diogo2007@gmail.com>
2024-05-21 19:59:56 +0100

    Add files via upload

---
+++ PARKOUR COM CUBOS/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1716317068,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"images/movimentodochao.png",
+		"images/tiledbackground4.png",
+		"images/informação.png",
+		"images/tiledbackground5.png",
+		"images/particles.png",
+		"images/tiledbackground6.png",
+		"images/tiledbackground7.png",
+		"images/tiledbackground8.png",
+		"images/particles2.png"
+	]
+}
